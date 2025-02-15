d6cd95b7a0dd5f09ceb6d6f20022be63263b36b9
Yuchen Wang <contact@wangyuchen.cn>
2025-02-15 17:35:41 +0800

    chore: add eslint

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  eslintPluginPrettierRecommended,
+];
