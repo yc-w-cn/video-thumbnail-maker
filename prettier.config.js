e122978a7c6c83d4df4331b9feb4c956e4788775
Yuchen Wang <contact@wangyuchen.cn>
2025-02-15 17:37:27 +0800

    chore: add prettier.config.js

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  "singleQuote": true,
+  "tabWidth": 2,
+  "printWidth": 80,
+}
+
+export default config;
